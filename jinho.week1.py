359797fa85398bf97dac2b5dbac7ff55e7453100
jinho-Kang <ryh268268@gmail.com>
2022-03-01 00:19:17 +0900

    week1 2.28

---
+++ jinho.week1.py
@@ -0,0 +1,10 @@
+import re
+Regex_Pattern = r'hackerrank'	# Do not delete 'r'.
+
+# \.이것은 점을 의미함 $은 문자열 종료?를 말하는걸로 알고있음.
+import re
+regex_pattern = r"...\....\....\....$"	# Do not delete 'r'.
+
+# 문제에서 x는 숫자,X는 문자라고했으니까
+import re
+Regex_Pattern = r"\d\d\D\d\d\D\d\d\d\d"	# Do not delete 'r'.
