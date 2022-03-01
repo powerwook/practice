e8f4174cffdde5286ded6e6521eaefe48768baa7
ThingSik1997 <99626816+ThingSik1997@users.noreply.github.com>
2022-03-01 18:30:21 +0900

    20220301

--- 정규표현식/week01/myeongsik.py
+++ 정규표현식/week01/myeongsik.py
@@ -1,5 +1,5 @@
-Regex_Pattern = r'hackerrank'
+1.Regex_Pattern = r"\S\S\s\S\S\s\S\S"
 
-Regex_pattern = r"...\....\....\....$"
+2.Regex_Pattern = r"\w\w\w\W\w\w\w\w\w\w\w\w\w\w\W\w\w\w"
 
-Regex_Pattern = r"\d\d.\d\d.\d\d\d\d"
+3.Regex_Pattern = r"^\d....\.$"
