cc3dfe428f8093958325e42be2f3a8d44b772b09
powerwook <powerwook@naver.com>
2022-03-02 11:02:25 +0900

    수정

---
+++ 알고리즘/그리디/문자열뒤집기_hyunwook.py
@@ -0,0 +1,10 @@
+S = input()
+zero_len = S.split('0')
+one_len = S.split('1')
+# 빈문자 리스트내에서 제거
+zero_len = len([x for x in zero_len if x])
+one_len = len([x for x in one_len if x])
+if zero_len >= one_len:
+    print(one_len)
+else:
+    print(zero_len)
