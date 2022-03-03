e2e1f5874ee75c75ec24f3d591b489ec0c8c4d13
jinho-Kang <ryh268268@gmail.com>
2022-03-03 23:42:15 +0900

    알고리즘5번,정규표현식문제품 3.3

--- 알고리즘/jinho 알고리즘/볼링공고르기jinhokang.py
+++ 알고리즘/jinho 알고리즘/볼링공고르기jinhokang.py
@@ -1,11 +1,12 @@
 import sys
 sys.stdin=open("input.txt","r")
-n,k=map(int,input().split())
+n,k=map(int,(input().split()))
 l=list(map(int,input().split()))
+
 cnt=0
+
 for i,v in enumerate(l):
     for j in l[i+1:]:
-        if v==j:
-            continue
-        else:cnt+=1
+        if v!=j:
+            cnt+=1
 print(cnt)
