b6f1033b926ebc064155e3f63f3113b560eea430
powerwook <powerwook@naver.com>
2022-03-03 20:49:52 +0900

    a

--- 알고리즘/그리디/곱하기혹은더하기_hyunwook.py
+++ 알고리즘/그리디/곱하기혹은더하기_hyunwook.py
@@ -1,8 +1,8 @@
 s = input()
 result = 0
-
+# 숫자 1일경우 더해주는게 이득
 for i in s:
-    if result == 0 or i == '0':
+    if result <= 1 or i == '0' or i == '1':
         result += int(i)
     else:
         result *= int(i)
