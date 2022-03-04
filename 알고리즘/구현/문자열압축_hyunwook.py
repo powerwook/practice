60472dc088cb4050da4e19ac2229d10940a370a1
powerwook <powerwook@naver.com>
2022-03-04 16:36:06 +0900

    알고리즘

---
+++ 알고리즘/구현/문자열압축_hyunwook.py
@@ -0,0 +1,21 @@
+#정답 코드를 확인한 문제
+def solution(s):
+    answer = len(s)
+    
+    for step in range(1, len(s) // 2 + 1):
+        compressed = ""
+        prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
+        count = 1
+        # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+        for j in range(step, len(s), step):
+            # 이전 상태와 동일하다면 압축 횟수(count) 증가
+            if prev == s[j:j + step]:
+                count += 1
+            else:
+                compressed += str(count) + prev if count >= 2 else prev
+                prev = s[j:j + step] # 다시 상태 초기화
+                count = 1
+
+        compressed += str(count) + prev if count >= 2 else prev
+        answer = min(answer, len(compressed))
+    return answer
