c2d262c270390c0a30a8116b24fb2c90477e82ab
powerwook <powerwook@naver.com>
2022-03-04 21:47:22 +0900

    정규표현식 마무리

--- 정규표현식/week01/hyunwook.py
+++ 정규표현식/week01/hyunwook.py
@@ -47,28 +47,87 @@ Regex_Pattern = r"^\d\w{4}\.$"
 
 # 3월 2일
 #1. **Matching Specific Characters**
+"""
+첫번째문자 1,2,3중하나
+두번째 1,2,0중하나
+세번째 x,s,0
+네번째 3 0 A a
+다섯번째 x s u
+여섯번째 . 이나 ,
+"""
 Regex_Pattern = r'^[123][120][xs0][30Aa][xsu][.,]$'
 #2. **Excluding Specific Characters**
-Regex_Pattern = r'^[^0-9][^aeiou][^bcDF]\S[^AEIOU][^.,]$'
+"""
+첫번째 0부터 9 아님 숫자아님
+두번째 aeiou 아님
+세번째 bcDF 아님
+네번째 공백문자 아님
+다섯번째 AEIOU 아님
+여섯번째 ., 아님
+"""
+Regex_Pattern = r'^\D[^aeiou][^bcDF]\S[^AEIOU][^.,]$'
 #3. **Matching Character Ranges**
+"""
+문자열의 길이가 5보다 커야하므로 앞5글자 이후에는 제한을 두지 않음
+첫번째 a부터 z
+두번째 1부터 9
+세번째 a부터 z 아님
+네번째 A부터 Z 아님
+다섯번째 A부터 Z
+"""
 Regex_Pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'
 
 # 3월 3일
 #1. **Matching Word Boundaries**
+"""
+\b는 경계, 범위? 를 지정하기 위해 사용함
+첫문자가 모음으로 시작하는 문자가 있는지 확인
+"""
 Regex_Pattern = r'\b[aeiouAEIOU][a-zA-Z]*\b'
 #2. **Capturing & Non-Capturing Groups**
+"""
+( )를 통해 문자를 그룹설정함
+ok가 3번이상 반복
+"""
 Regex_Pattern = r'(ok){3,}'	
 #3. **Alternative Matching**
+"""
+| 는 or 연산
+[a-zA-Z]가 1번이상 반복
+"""
 Regex_Pattern = r'^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$'
 
 # 3월 4일
 #1. **Matching {x} Repetitions**
+"""
+앞뒤 글자수 제한
+정해진 조건에 대해 40번 반복
+5번반복
+"""
 Regex_Pattern = r'^[a-zA-Z02468]{40}[13579\s]{5}$'
 #2. **Matching {x, y} Repetitions**
+"""
+앞뒤 글자수 제한
+1~2번 반복
+3번이상 반복
+0~3번 반복(0은 생략가능)
+"""
 Regex_Pattern = r'^\w{1,2}[a-zA-Z]{3,}\W{,3}$'
 #3. **Matching Zero Or More Repetitions**
+"""
+앞뒤 글자수 제한
+2번이상 반복
+* -> 0번또는 그이상 반복
+"""
 Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'
 #4. **Matching One Or More Repetitions**
+"""
++ 앞의 문자는 1번이상 반복
+"""
 Regex_Pattern = r'^\d+[A-Z]+[a-z]+$'
 #5. **Matching Ending Items**
-Regex_Pattern = r'^[a-zA-Z]*s$'
+"""
+앞의 그룹문자 0번 또는 그이상반복
+마지막문자 s
+"""
+Regex_Pattern = r'^[a-zA-Z]*s$' 
--- 정규표현식/week02/hyunwook.py
+++ 정규표현식/week02/hyunwook.py
@@ -2,16 +2,56 @@
 #1. Branch Reset Groups -> 해당 언어가 없어서 스킵
 
 #2. Matching Same Text Again & Again
+"""
+20글자를 위해 앞뒤에 ^와 $ c처리를 하고 20글자의 표현식을 작성하였고 조건에 따라
+1캐릭터: lowercase letter.
+2캐릭터: word character.
+3캐릭터: whitespace character.
+4캐릭터: non word character.
+5캐릭터: digit.
+6캐릭터: non digit.
+7캐릭터: uppercase letter.
+8문자: letter(소문자 또는 대문자).
+9문자: vowel(a, e, i , o , u, A, E, I, O 또는 U).
+10캐릭터: non whitespace character.
+11문자: 와 같아야 합니다 1st character.
+12문자: 와 같아야 합니다 2nd character.
+13문자: 와 같아야 합니다 3rd character.
+14문자: 와 같아야 합니다 4th character.
+15문자: 와 같아야 합니다 5th character.
+16문자: 와 같아야 합니다 6th character.
+17문자: 와 같아야 합니다 7th character.
+18문자: 와 같아야 합니다 8th character.
+19문자: 와 같아야 합니다 9th character.
+20문자: 와 같아야 합니다 10th character.
+"""
 Regex_Pattern = r'([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10'
 #3. Backreferences To Failed Groups
+"""
+8자리구성을 위해 ^ $ 처리
+-가 들어갈 수도 있고 안들어갈수도 있어서 ?처리로 들어가면 다들어가고
+안들어가면 안들어가게 처리
+"""
 Regex_Pattern = r"^\d\d(-?)\d\d\1\d\d\1\d\d$"
 #4. Forward References -> 파이썬은 스킵, 자바는 풀이 진행
 
 #5. Positive Lookahead
+"""
+o문자 다음에 oo문자가 나오는 횟수
+"""
 Regex_Pattern = r'o(?=oo)'
 #6. Negative Lookahead
+"""
+임의의 한자리수 문자표현, 뒤에 오는 문자와 일치하지 않는 것 카운트
+"""
 Regex_Pattern = r"(.)(?!\1)"
 #7. Positive Lookbehind
+"""
+숫자이고 앞자리가 홀수인 숫자가 나오는 카운트
+"""
 Regex_Pattern = r"(?<=[13579])\d"
 #8. Negative Lookbehind
+"""
+임의의 한자리수 문자이고 그 앞에 영어 모음이 나오지 않는 카운트
+"""
 Regex_Pattern = r"(?<![aeiouAEIOU])."
